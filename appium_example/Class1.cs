8b04c56e82353b375955761d2baffb1215f7ab13
Alexey Spiridenok <alexey.spiridenok@helmes.ee>
2019-05-24 16:30:28 +0300

    Add project files.

---
+++ appium_example/Class1.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppiumExample
+{
+    public class Class1
+    {
+        IWebDriver driver;
+
+
+        [SetUp]
+        public void InitDriver()
+        {
+            driver = new ChromeDriver("D:\\Work\\SharpProject\\Tools");
+        }
+
+        [Test]
+        public void test()
+        {
+            driver.Url = "https://www.google.com/";
+            driver.FindElement(XPathLookupException)
+        }
+
+
+        [TearDown]
+        public void closeBrowser()
+        {
+            driver.Close();
+        }
+
+    }
+}
